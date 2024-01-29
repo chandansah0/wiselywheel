d66c147b27a287467cdae18b4f202354126d5e67
Siddhant Rai <105770390+Siddhant20020@users.noreply.github.com>
2024-01-29 18:39:57 +0545

    Frontend

--- Frontend/aboutus.js
+++
@@ -1,7 +0,0 @@
-//for about us
-function toggleAboutSection() {
-    const aboutSection = document.querySelector('.about-us');
-    if (aboutSection) {
-        aboutSection.classList.toggle('visible');
-    }
-}
