0a0a911caeb2be0d0b497aa8304750d9efb20d53
Siddhant Rai <105770390+Siddhant20020@users.noreply.github.com>
2024-02-05 19:14:15 +0545

    back

--- server/routes/bikes.js
+++ server/routes/bikes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const BikeModel = require('../models/bike');
+const BikeModel = require('../model/bikes');
 
 router.get('/', async (req, res) => {
   try {
