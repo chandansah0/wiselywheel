b679564069c711c266fda977bc06109368e3d0b3
Siddhant Rai <105770390+Siddhant20020@users.noreply.github.com>
2024-02-17 10:35:24 +0545

    style

--- server/model/bikes.js
+++ server/model/bikes.js
@@ -1,28 +1,23 @@
 const mongoose = require('mongoose');
 
 const bikefeatureSchema = new mongoose.Schema({
-  brand: String,
   variant_name: String,
   price: Number,
   engine_type: String,
   displacement: String,
   max_torque: String,
   cooling_system: String,
-  valve_per_cylinder: Number,
+  valve_per_cylinder: String,
   starting: String,
   fuel_supply: String,
   clutch: String,
   ignition: String,
-  gear_box: String,
-  bore: String,
-  stroke: String,
-  city_mileage: String,
+  mileage: String,
   body_type: String,
-  acceleration_time: String,
+  zero_to_100: String,
   peak_power: String,
-  front_suspension: String,
   transmission: String,
-  braking_system: String
+  brand: String
 });
 
 const BikeFeature = mongoose.model('BikeFeature', bikefeatureSchema);
